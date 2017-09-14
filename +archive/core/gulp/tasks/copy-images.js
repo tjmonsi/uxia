a99a7fda784149e0d10551d71e7a457c451a8f19
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-09-14 18:36:49 +0800

    made it static
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- +archive/core/gulp/tasks/copy-images.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp')
-const {destinationFolder, sources} = require('../utils/utils')
-
-gulp.task('copy-images', () => {
-  return gulp.src(sources.images)
-    .pipe(gulp.dest(`${destinationFolder()}/images`))
-})
--- src/routing.js
+++
@@ -1,6 +0,0 @@
-
-    export default {
-      '/': () => { return import(/* webpackChunkName: "uxia-home-page" */ './modules/uxia-module/pages/uxia-home-page/uxia-home-page.html') }, 
-'style-guide': () => { return import(/* webpackChunkName: "example-style-guide-page" */ './modules/example-module/pages/example-style-guide-page/example-style-guide-page.js') }
-    }
-  
