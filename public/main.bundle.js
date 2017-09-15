e2bc4c8fb0f5d1edc31319b8f3130dc0fe04b35e
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-09-15 02:55:52 +0000

    Changed from Montserrat to Open Sans

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -7812,12 +7812,13 @@ __webpack_require__(22);
         d = new n();d.start = 0;d.end = c.length;for (var e = d, f = 0, h = c.length; f < h; f++) {
       if ("{" === c[f]) {
         e.rules || (e.rules = []);var g = e,
-            m = g.rules[g.rules.length - 1] || null;e = new n();e.start = f + 1;e.parent = g;e.previous = m;g.rules.push(e);
+            m = g.rules[g.rules.length - 1] || null,
+            e = new n();e.start = f + 1;e.parent = g;e.previous = m;g.rules.push(e);
       } else "}" === c[f] && (e.end = f + 1, e = e.parent || d);
     }return b(d, a);
   }
   function q(a, b) {
-    var c = b.substring(a.start, a.end - 1);a.parsedCssText = a.cssText = c.trim();a.parent && (c = b.substring(a.previous ? a.previous.end : a.parent.start, a.start - 1), c = ca(c), c = c.replace(r, " "), c = c.substring(c.lastIndexOf(";") + 1), c = a.parsedSelector = a.selector = c.trim(), a.atRule = 0 === c.indexOf("@"), a.atRule ? 0 === c.indexOf("@media") ? a.type = t : c.match(da) && (a.type = u, a.keyframesName = a.selector.split(r).pop()) : a.type = 0 === c.indexOf("--") ? v : x);if (c = a.rules) for (var d = 0, e = c.length, f; d < e && (f = c[d]); d++) {
+    var c = b.substring(a.start, a.end - 1);a.parsedCssText = a.cssText = c.trim();a.parent && ((c = b.substring(a.previous ? a.previous.end : a.parent.start, a.start - 1), c = ca(c), c = c.replace(r, " "), c = c.substring(c.lastIndexOf(";") + 1), c = a.parsedSelector = a.selector = c.trim(), a.atRule = !c.indexOf("@"), a.atRule) ? c.indexOf("@media") ? c.match(da) && (a.type = u, a.keyframesName = a.selector.split(r).pop()) : a.type = t : a.type = c.indexOf("--") ? v : x);if (c = a.rules) for (var d = 0, e = c.length, f; d < e && (f = c[d]); d++) {
       q(f, b);
     }return a;
   }
@@ -7835,57 +7836,55 @@ __webpack_require__(22);
         f = 0;for (var h = e.length, g; f < h && (g = e[f]); f++) {
           d = y(g, b, d);
         }
-      } else b ? b = a.cssText : (b = a.cssText, b = b.replace(ea, "").replace(fa, ""), b = b.replace(ha, "").replace(ia, "")), (d = b.trim()) && (d = "  " + d + "\n");
+      } else b ? b = a.cssText : (b = a.cssText, b = b.replace(ea, "").replace(fa, ""), b = b.replace(ga, "").replace(ha, "")), (d = b.trim()) && (d = "  " + d + "\n");
     }d && (a.selector && (c += a.selector + " {\n"), c += d, a.selector && (c += "}\n\n"));return c;
   }
-  var x = 1,
+  var v = 1,
       u = 7,
       t = 4,
-      v = 1E3,
+      x = 1E3,
       aa = /\/\*[^*]*\*+([^/*][^*]*\*+)*\//gim,
       ba = /@import[^;]*;/gim,
       ea = /(?:^[^;\-\s}]+)?--[^;{}]*?:[^{};]*?(?:[;\n]|$)/gim,
       fa = /(?:^[^;\-\s}]+)?--[^;{}]*?:[^{};]*?{[^}]*?}(?:[;\n]|$)?/gim,
-      ha = /@apply\s*\(?[^);]*\)?\s*(?:[;\n]|$)?/gim,
-      ia = /[^;:]*?:[^;]*?var\([^;]*\)(?:[;\n]|$)?/gim,
+      ga = /@apply\s*\(?[^);]*\)?\s*(?:[;\n]|$)?/gim,
+      ha = /[^;:]*?:[^;]*?var\([^;]*\)(?:[;\n]|$)?/gim,
       da = /^@[^\s]*keyframes/,
-      r = /\s+/g;var ja = Promise.resolve();function ka(a) {
+      r = /\s+/g;var ia = Promise.resolve();function ja(a) {
     if (a = k[a]) a._applyShimCurrentVersion = a._applyShimCurrentVersion || 0, a._applyShimValidatingVersion = a._applyShimValidatingVersion || 0, a._applyShimNextVersion = (a._applyShimNextVersion || 0) + 1;
   }function z(a) {
     return a._applyShimCurrentVersion === a._applyShimNextVersion;
-  }function la(a) {
-    a._applyShimValidatingVersion = a._applyShimNextVersion;a.a || (a.a = !0, ja.then(function () {
+  }function ka(a) {
+    a._applyShimValidatingVersion = a._applyShimNextVersion;a.a || (a.a = !0, ia.then(function () {
       a._applyShimCurrentVersion = a._applyShimNextVersion;a.a = !1;
     }));
-  };var A = !(window.ShadyDOM && window.ShadyDOM.inUse),
-      B;function C(a) {
-    B = a && a.shimcssproperties ? !1 : A || !(navigator.userAgent.match(/AppleWebKit\/601|Edge\/15/) || !window.CSS || !CSS.supports || !CSS.supports("box-shadow", "0 0 0 var(--foo)"));
-  }window.ShadyCSS && void 0 !== window.ShadyCSS.nativeCss ? B = window.ShadyCSS.nativeCss : window.ShadyCSS ? (C(window.ShadyCSS), window.ShadyCSS = void 0) : C(window.WebComponents && window.WebComponents.flags);var E = B;var F = /(?:^|[;\s{]\s*)(--[\w-]*?)\s*:\s*(?:((?:'(?:\\'|.)*?'|"(?:\\"|.)*?"|\([^)]*?\)|[^};{])+)|\{([^}]*)\}(?:(?=[;\s}])|$))/gi,
-      G = /(?:^|\W+)@apply\s*\(?([^);\n]*)\)?/gi,
-      ma = /@media\s(.*)/;function H(a) {
-    if (!a) return "";"string" === typeof a && (a = p(a));return y(a, E);
+  };var A = /(?:^|[;\s{]\s*)(--[\w-]*?)\s*:\s*(?:((?:'(?:\\'|.)*?'|"(?:\\"|.)*?"|\([^)]*?\)|[^};{])+)|\{([^}]*)\}(?:(?=[;\s}])|$))/gi,
+      B = /(?:^|\W+)@apply\s*\(?([^);\n]*)\)?/gi,
+      la = /@media\s(.*)/;var C = !(window.ShadyDOM && window.ShadyDOM.inUse),
+      E;function F(a) {
+    E = a && a.shimcssproperties ? !1 : C || !(navigator.userAgent.match(/AppleWebKit\/601|Edge\/15/) || !window.CSS || !CSS.supports || !CSS.supports("box-shadow", "0 0 0 var(--foo)"));
+  }window.ShadyCSS && void 0 !== window.ShadyCSS.nativeCss ? E = window.ShadyCSS.nativeCss : window.ShadyCSS ? (F(window.ShadyCSS), window.ShadyCSS = void 0) : F(window.WebComponents && window.WebComponents.flags);var G = E;function H(a) {
+    if (!a) return "";"string" === typeof a && (a = p(a));return y(a, G);
   }function I(a) {
     !a.__cssRules && a.textContent && (a.__cssRules = p(a.textContent));return a.__cssRules || null;
   }function J(a, b, c, d) {
     if (a) {
       var e = !1,
           f = a.type;if (d && f === t) {
-        var h = a.selector.match(ma);h && (window.matchMedia(h[1]).matches || (e = !0));
-      }f === x ? b(a) : c && f === u ? c(a) : f === v && (e = !0);if ((a = a.rules) && !e) {
-        e = 0;f = a.length;for (var g; e < f && (g = a[e]); e++) {
-          J(g, b, c, d);
-        }
+        var h = a.selector.match(la);h && (window.matchMedia(h[1]).matches || (e = !0));
+      }f === v ? b(a) : c && f === u ? c(a) : f === x && (e = !0);if ((a = a.rules) && !e) for (var e = 0, f = a.length, g; e < f && (g = a[e]); e++) {
+        J(g, b, c, d);
       }
     }
   }
   function K(a, b) {
     var c = a.indexOf("var(");if (-1 === c) return b(a, "", "", "");a: {
       var d = 0;var e = c + 3;for (var f = a.length; e < f; e++) {
-        if ("(" === a[e]) d++;else if (")" === a[e] && 0 === --d) break a;
+        if ("(" === a[e]) d++;else if (")" === a[e] && ! --d) break a;
       }e = -1;
     }d = a.substring(c + 4, e);c = a.substring(0, c);a = K(a.substring(e + 1), b);e = d.indexOf(",");return -1 === e ? b(c, d.trim(), "", a) : b(c, d.substring(0, e).trim(), d.substring(e + 1).trim(), a);
-  };var na = /;\s*/m,
-      oa = /^\s*(initial)|(inherit)\s*$/;function L() {
+  };var ma = /;\s*/m,
+      na = /^\s*(initial)|(inherit)\s*$/;function L() {
     this.a = {};
   }L.prototype.set = function (a, b) {
     a = a.trim();this.a[a] = { h: b, i: {} };
@@ -7894,7 +7893,7 @@ __webpack_require__(22);
   };var M = null;function N() {
     this.b = this.c = null;this.a = new L();
   }N.prototype.o = function (a) {
-    a = G.test(a) || F.test(a);G.lastIndex = 0;F.lastIndex = 0;return a;
+    a = B.test(a) || A.test(a);B.lastIndex = 0;A.lastIndex = 0;return a;
   };N.prototype.m = function (a, b) {
     a = a.content.querySelector("style");var c = null;a && (c = this.j(a, b));return c;
   };
@@ -7910,32 +7909,34 @@ __webpack_require__(22);
       c.g(a);
     });this.c = null;
   };N.prototype.g = function (a) {
-    a.cssText = pa(this, a.parsedCssText);":root" === a.selector && (a.selector = ":host > *");
+    a.cssText = oa(this, a.parsedCssText);":root" === a.selector && (a.selector = ":host > *");
   };
-  function pa(a, b) {
-    b = b.replace(F, function (b, d, e, f) {
-      return qa(a, b, d, e, f);
+  function oa(a, b) {
+    b = b.replace(A, function (b, d, e, f) {
+      return pa(a, b, d, e, f);
     });return O(a, b);
   }function O(a, b) {
-    for (var c; c = G.exec(b);) {
+    for (var c; c = B.exec(b);) {
       var d = c[0],
-          e = c[1];c = c.index;var f = b.slice(0, c + d.indexOf("@apply"));b = b.slice(c + d.length);var h = P(a, f);d = void 0;var g = a;e = e.replace(na, "");var m = [];var l = g.a.get(e);l || (g.a.set(e, {}), l = g.a.get(e));if (l) for (d in g.c && (l.i[g.c] = !0), l.h) {
+          e = c[1];c = c.index;var f = b.slice(0, c + d.indexOf("@apply"));b = b.slice(c + d.length);var h = P(a, f),
+          d = void 0;var g = a;var e = e.replace(ma, ""),
+          m = [];var l = g.a.get(e);l || (g.a.set(e, {}), l = g.a.get(e));if (l) for (d in g.c && (l.i[g.c] = !0), l.h) {
         g = h && h[d], l = [d, ": var(", e, "_-_", d], g && l.push(",", g), l.push(")"), m.push(l.join(""));
-      }d = m.join("; ");b = "" + f + d + b;G.lastIndex = c + d.length;
+      }d = m.join("; ");b = "" + f + d + b;B.lastIndex = c + d.length;
     }return b;
   }
   function P(a, b) {
     b = b.split(";");for (var c, d, e = {}, f = 0, h; f < b.length; f++) {
       if (c = b[f]) if (h = c.split(":"), 1 < h.length) {
-        c = h[0].trim();var g = a;d = c;h = h.slice(1).join(":");var m = oa.exec(h);m && (m[1] ? (g.b || (g.b = document.createElement("meta"), g.b.setAttribute("apply-shim-measure", ""), g.b.style.all = "initial", document.head.appendChild(g.b)), d = window.getComputedStyle(g.b).getPropertyValue(d)) : d = "apply-shim-inherit", h = d);d = h;e[c] = d;
+        c = h[0].trim();var g = a;d = c;h = h.slice(1).join(":");var m = na.exec(h);m && (m[1] ? (g.b || (g.b = document.createElement("meta"), g.b.setAttribute("apply-shim-measure", ""), g.b.style.all = "initial", document.head.appendChild(g.b)), d = window.getComputedStyle(g.b).getPropertyValue(d)) : d = "apply-shim-inherit", h = d);d = h;e[c] = d;
       }
     }return e;
-  }function ra(a, b) {
+  }function qa(a, b) {
     if (M) for (var c in b.i) {
       c !== a.c && M(c);
     }
   }
-  function qa(a, b, c, d, e) {
+  function pa(a, b, c, d, e) {
     d && K(d, function (b, c) {
       c && a.a.get(c) && (e = "@apply " + c + ";");
     });if (!e) return b;var f = O(a, e),
@@ -7946,7 +7947,7 @@ __webpack_require__(22);
         w,
         Y = !1;for (w in g) {
       var D = f[w];void 0 === D && (D = "initial");!l || w in l || (Y = !0);X.push("" + c + "_-_" + w + ": " + D);
-    }Y && ra(a, m);m && (m.h = g);d && (h = b + ";" + h);return "" + h + X.join("; ") + ";";
+    }Y && qa(a, m);m && (m.h = g);d && (h = b + ";" + h);return "" + h + X.join("; ") + ";";
   }N.prototype.detectMixin = N.prototype.o;N.prototype.transformStyle = N.prototype.j;
   N.prototype.transformCustomStyle = N.prototype.f;N.prototype.transformRules = N.prototype.l;N.prototype.transformRule = N.prototype.g;N.prototype.transformTemplate = N.prototype.m;N.prototype._separator = "_-_";Object.defineProperty(N.prototype, "invalidCallback", { get: function get() {
       return M;
@@ -7954,7 +7955,7 @@ __webpack_require__(22);
       M = a;
     } });var Q = null,
       R = window.HTMLImports && window.HTMLImports.whenReady || null,
-      S;function sa(a) {
+      S;function ra(a) {
     requestAnimationFrame(function () {
       R ? R(a) : (Q || (Q = new Promise(function (a) {
         S = a;
@@ -7965,9 +7966,9 @@ __webpack_require__(22);
       }));
     });
   };var T = new N();function U() {
-    var a = this;this.a = null;sa(function () {
+    var a = this;this.a = null;ra(function () {
       V(a);
-    });T.invalidCallback = ka;
+    });T.invalidCallback = ja;
   }function V(a) {
     a.a || (a.a = window.ShadyCSS.CustomStyleInterface, a.a && (a.a.transformCallback = function (a) {
       T.f(a);
@@ -7999,14 +8000,14 @@ __webpack_require__(22);
   U.prototype.styleElement = function (a) {
     V(this);var b = a.localName,
         c;b ? -1 < b.indexOf("-") ? c = b : c = a.getAttribute && a.getAttribute("is") || "" : c = a.is;if ((b = k[c]) && !z(b)) {
-      if (z(b) || b._applyShimValidatingVersion !== b._applyShimNextVersion) this.prepareTemplate(b, c), la(b);if (a = a.shadowRoot) if (a = a.querySelector("style")) a.__cssRules = b._styleAst, a.textContent = H(b._styleAst);
+      if (z(b) || b._applyShimValidatingVersion !== b._applyShimNextVersion) this.prepareTemplate(b, c), ka(b);if (a = a.shadowRoot) if (a = a.querySelector("style")) a.__cssRules = b._styleAst, a.textContent = H(b._styleAst);
     }
   };U.prototype.styleDocument = function (a) {
     V(this);this.styleSubtree(document.body, a);
   };
   if (!window.ShadyCSS || !window.ShadyCSS.ScopingShim) {
     var Z = new U(),
-        ta = window.ShadyCSS && window.ShadyCSS.CustomStyleInterface;window.ShadyCSS = { prepareTemplate: function prepareTemplate(a, b) {
+        sa = window.ShadyCSS && window.ShadyCSS.CustomStyleInterface;window.ShadyCSS = { prepareTemplate: function prepareTemplate(a, b) {
         W(Z);Z.prepareTemplate(a, b);
       }, styleSubtree: function styleSubtree(a, b) {
         W(Z);Z.styleSubtree(a, b);
@@ -8016,7 +8017,7 @@ __webpack_require__(22);
         W(Z);Z.styleDocument(a);
       }, getComputedStyleValue: function getComputedStyleValue(a, b) {
         return (a = window.getComputedStyle(a).getPropertyValue(b)) ? a.trim() : "";
-      }, nativeCss: E, nativeShadow: A };ta && (window.ShadyCSS.CustomStyleInterface = ta);
+      }, nativeCss: G, nativeShadow: C };sa && (window.ShadyCSS.CustomStyleInterface = sa);
   }window.ShadyCSS.ApplyShim = T;
 }).call(undefined);
 
